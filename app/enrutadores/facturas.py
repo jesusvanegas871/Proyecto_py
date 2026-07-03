bb9c5a067f90789e0b7f5e7faa90d1fb300bda21
JesusVanegas-7 <vanegaslordjesus444@gmail.com>
2026-07-03 13:46:07 -0500

    facturas bd

--- app/enrutadores/facturas.py
+++ app/enrutadores/facturas.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, HTTPException, status
 from ..modelos.facturas import Factura, FacturaCrear, FacturaEditar
 from ..modelos.clientes import Cliente
 from ..listas import lista_clientes, lista_facturas
+from ..conexion_bd import Sesion_dependencia
+from sqlmodel import select
 
 rutas_facturas = APIRouter()
 
@@ -10,7 +12,9 @@ rutas_facturas = APIRouter()
 
 
 @rutas_facturas.get("/facturas", response_model=list[Factura])
-async def listar_facturas():
+async def listar_facturas(sesion: Sesion_dependencia):
+    consulta = select(Factura)
+    lista_facturas = sesion.exec(consulta).all()
     return lista_facturas
 
 
@@ -26,19 +30,33 @@ async def listar_factura(factura_id: int):
         detail=f"La factura con id {factura_id}, no existe."
     )
 
+@rutas_facturas.post("/facturas/{cliente_id}", response_model=Factura)
+async def crear_factura(
+    cliente_id: int,
+    datos_factura: FacturaCrear,
+    sesion: Sesion_dependencia
+):
+    # buscar el cliente en bd
+    cliente_encontrado = sesion.get(Cliente, cliente_id)
 
-@rutas_facturas.post("/facturas", response_model=Factura)
-async def crear_factura(datos_factura: FacturaCrear):
-    factura_val = Factura.model_validate(datos_factura.model_dump())
+    # mensaje si no existe el cliente
+    if not cliente_encontrado:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"El cliente con id {cliente_id}, no existe.",
+        )
 
-    # generar id
-    id_factura = len(lista_facturas) + 1
-    factura_val.id = id_factura
+    # validar datos de la factura-json, pasar dict
+    factura_dict = datos_factura.model_dump()
+    factura_dict["cliente_id"] = cliente_id
+    factura_val = Factura.model_validate(factura_dict)
 
-    lista_facturas.append(factura_val)
+    # guardar en bd
+    sesion.add(factura_val)
+    sesion.commit()
+    sesion.refresh(factura_val)
 
     return factura_val
-
 
 @rutas_facturas.patch("/facturas/{factura_id}", response_model=Factura)
 async def editar_factura(factura_id: int, datos_factura: FacturaEditar):
